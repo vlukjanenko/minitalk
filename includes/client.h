5f449ee12a509966a975a2b67026d7ab2c01d610
majosue <vlukjanenko@mail.ru>
2021-11-18 17:58:01 +0300

    norm complete

--- includes/client.h
+++ includes/client.h
@@ -6,7 +6,7 @@
 /*   By: majosue <majosue@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 17:29:38 by majosue           #+#    #+#             */
-/*   Updated: 2021/11/18 14:34:46 by majosue          ###   ########.fr       */
+/*   Updated: 2021/11/18 17:56:10 by majosue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,12 @@
 # include "libft.h"
 # include <signal.h>
 
-#define ZERO		SIGUSR1
+# define ZERO	SIGUSR1
 
 struct s_message
 {
 	int			pid;
 	const char	*str;
 };
-
 
 #endif
--- includes/server.h
+++ includes/server.h
@@ -6,7 +6,7 @@
 /*   By: majosue <majosue@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 17:29:38 by majosue           #+#    #+#             */
-/*   Updated: 2021/11/18 14:31:06 by majosue          ###   ########.fr       */
+/*   Updated: 2021/11/18 17:57:28 by majosue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 # include "libft.h"
 # include <signal.h>
 
-#define ZERO		SIGUSR1
-#define ONE			SIGUSR2
-#define NEXT		SIGUSR1
-#define DONE		SIGUSR2
-#define MESSAGE_LEN	2048
+# define ZERO		SIGUSR1
+# define ONE			SIGUSR2
+# define NEXT		SIGUSR1
+# define DONE		SIGUSR2
+# define MESSAGE_LEN	2048
 
 struct s_message
 {
@@ -31,15 +31,14 @@ struct s_message
 struct s_char
 {
 	char	c;
-	uint8_t bits;
+	uint8_t	bits;
 };
 
-void	reset_char(struct s_char* current_char);
+void	reset_char(struct s_char *current_char);
 void	reset_message(struct s_message *message);
 void	enlarge_message(struct s_message *message);
 void	add_char(struct s_message *message, char c);
 int		set_pid(int current_pid, int pid, struct s_char *current_char, \
 											struct s_message *message);
-
 
 #endif
